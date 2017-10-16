2777154261654087813eafce2cf55ad3c8b506f7
Rooster212 <jamieroos1@gmail.com>
2017-10-16 21:57:38 +0100

    Move to NUnit

--- DotNetStandardCalculatorTests/StandardCalculatorTests.cs
+++ DotNetStandardCalculatorTests/StandardCalculatorTests.cs
@@ -1,43 +1,43 @@
-using Xunit;
+using NUnit.Framework;
 
 namespace DotNetStandardCalculatorTests
 {
     public class StandardCalculatorTests
     {
-        [Theory]
-        [InlineData("1 + 2", 3)]
-        [InlineData("3 * 2", 6)]
-        [InlineData("6 / 2", 3)]
-        [InlineData("6 - 3", 3)]
+        [Test]
+        [TestCase("1 + 2", 3)]
+        [TestCase("3 * 2", 6)]
+        [TestCase("6 / 2", 3)]
+        [TestCase("6 - 3", 3)]
         public void SimpleCalculator(string infixSum, double expected)
         {
             TestCommon.AssertCalculateEqualsValue(infixSum, expected);
         }
 
-        [Theory]
-        [InlineData("10 + 3 * 5", 25)]
-        [InlineData("10 / 2 + 5", 10)]
-        [InlineData("10 / 2 + 5 * 10", 55)]
+        [Test]
+        [TestCase("10 + 3 * 5", 25)]
+        [TestCase("10 / 2 + 5", 10)]
+        [TestCase("10 / 2 + 5 * 10", 55)]
         public void OperatorPrecedenceWorks(string infixSum, double expected)
         {
             TestCommon.AssertCalculateEqualsValue(infixSum, expected);
         }
 
-        [Theory]
-        [InlineData("(10 / 2) + 5", 10)]
-        [InlineData("(10 + 2) / 2", 6)]
-        [InlineData("10 * (60 + 3)", 630)]
-        [InlineData("10 * (60 - 3)", 570)]
-        [InlineData("(10 + 50) * (60 - 10)", 3000)]
+        [Test]
+        [TestCase("(10 / 2) + 5", 10)]
+        [TestCase("(10 + 2) / 2", 6)]
+        [TestCase("10 * (60 + 3)", 630)]
+        [TestCase("10 * (60 - 3)", 570)]
+        [TestCase("(10 + 50) * (60 - 10)", 3000)]
         public void ParentheseWorkAsExpected(string infixSum, double expected)
         {
             TestCommon.AssertCalculateEqualsValue(infixSum, expected);
         }
 
-        [Theory]
-        [InlineData("4^3 * (60 - 3)", 3648)]
-        [InlineData("4^2 * (6^2 - 3)", 528)]
-        [InlineData("2^2 + 3", 7)]
+        [Test]
+        [TestCase("4^3 * (60 - 3)", 3648)]
+        [TestCase("4^2 * (6^2 - 3)", 528)]
+        [TestCase("2^2 + 3", 7)]
         public void PowerOfOperatorWorks(string infixSum, double expected)
         {
             TestCommon.AssertCalculateEqualsValue(infixSum, expected);
